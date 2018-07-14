7d39ccfa1da0ecd0d939fdedba9724709e283a6a
iSNGlobalSolutions <40194649+iSNGlobalSolutions@users.noreply.github.com>
2018-07-14 10:42:59 +0530

    eSenate AndroidMobile v1.0

---
+++ app/src/main/java/app/com/esenatenigeria/room/RoomDb.java
@@ -0,0 +1,25 @@
+package app.com.esenatenigeria.room;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+import app.com.esenatenigeria.model.ActsModel;
+import app.com.esenatenigeria.model.BillsModel;
+import app.com.esenatenigeria.model.CommitteeModel;
+import app.com.esenatenigeria.model.InformationModel;
+import app.com.esenatenigeria.model.SenatorDetailModel;
+
+/**
+ * Created by Shubham verma on 25-04-2018.
+ */
+@Database(entities = {ActsModel.DataBean.class,
+        BillsModel.DataBean.class,
+        CommitteeModel.DataBean.class,
+        SenatorDetailModel.DataBean.class,
+        InformationModel.DataBean.class}, version = 1)
+@TypeConverters({MemberTypeConverters.class})
+public abstract class RoomDb extends RoomDatabase {
+    public abstract DaoAccess daoAccess();
+}
+
